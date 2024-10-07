f1c9702cf76c5f21274a0bcdd31d5226f5d34bf2
DANIELXXOMG <150471793+DANIELXXOMG2@users.noreply.github.com>
2024-10-07 13:49:49 -0500

    Delete Personal_Portafolio directory

--- Personal_Portafolio/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const themeCheckbox = document.querySelector('.theme-checkbox');
-    themeCheckbox.addEventListener('change', () => {
-        document.body.classList.toggle('dark-mode', themeCheckbox.checked);
-    });
-});
